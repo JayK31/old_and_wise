590766ce1ebf8b27415dd86ba593b5235c48b34c
JayK31 <jay.kaye31@gmail.com>
2014-12-19 08:06:24 -0500

    reject quote if same quote or from same author as previous

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,17 +3,21 @@ $(function() {
   $("#rando-quote-button").click(function(e) {
     e.preventDefault();
     $("#intro").hide();
+    var authorId = $("#author").data("authorId");
+    var quoteId = $("#quote").data("quoteId");
     $.ajax({
-      url: "/quotes",
+      url: "/quotes?author_id="+authorId+"&quote_id="+quoteId,
       method: "GET",
       dataType: "json",
     }).done(function(data) {
       var quote = data[0].text;
+      var quote_id = data[0].id;
       var author = data[1].name;
+      var author_id = data[1].id;
       var wiki = data[1].wiki_desc;
       var url = data[1].wiki_url;
-      $("#quote").text(quote);
-      $("#author").text(author);
+      $("#quote").text(quote).data("quoteId", quote_id);
+      $("#author").text(author).data("authorId", author_id);
       $("#author-name").text(author);
       $("#wiki").text(wiki.substring(0, 500)+"...");
       $("[data-author='wiki']").attr("href", url).text("wiki/"+author);
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,10 +1,18 @@
 class QuotesController < ApplicationController
   def index
     @quote = Quote.all.sample
+    if author_or_quote_present?
+      @quote = Quote.where("id != ?", params[:quote_id]).where("author_id != ?", params[:author_id]).sample
+    end
 
     respond_to do |format|
       format.html
       format.json { render json: [@quote, @quote.author] }
     end
+  end
+
+  private
+  def author_or_quote_present?
+    return true if params[:author_id].present? || params[:quote_id].present?
   end
 end
